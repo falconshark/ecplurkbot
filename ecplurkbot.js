2eba7eb5962c782ad4abc8bc85f6d440a8142448
Sardo <sardo.ip@sardo.work>
2015-09-17 23:51:17 +0800

    Will get config from nconf

--- ecplurkbot.js
+++ ecplurkbot.js
@@ -2,6 +2,14 @@ var log4js = require('log4js');
 var plurk = require('plurk');
 var nconf = require('nconf');
 
-log4js.clearAppenders();
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file(config['log'].log_path), 'BOT_LOG');
+nconf.file('config', __dirname + '/config/config.json')
+  .file('keywords', __dirname + '/config/keywords.json');
+
+var logging = nconf.get('log').logging;
+var log_path = nconf.get('log').log_path;
+
+if (logging) {
+  log4js.clearAppenders();
+  log4js.loadAppender('file');
+  log4js.addAppender(log4js.appenders.file(log_path), 'BOT_LOG');
+}
