980af6273f13aa850ada6e97c9f41430e5cff7f6
sardo.ip <sardo.ip@nxtomo.hk>
2015-09-18 19:38:50 +0800

    Will auto run checkTL function

--- ecplurkbot.js
+++ ecplurkbot.js
@@ -1,15 +1,21 @@
 var log4js = require('log4js');
-var plurk = require('plurk');
+var PlurkClient = require('plurk').PlurkClient;
 var nconf = require('nconf');
 
 nconf.file('config', __dirname + '/config/config.json')
-  .file('keywords', __dirname + '/config/keywords.json');
+	.file('keywords', __dirname + '/config/keywords.json');
 
 var logging = nconf.get('log').logging;
 var log_path = nconf.get('log').log_path;
 
 if (logging) {
-  log4js.clearAppenders();
-  log4js.loadAppender('file');
-  log4js.addAppender(log4js.appenders.file(log_path), 'BOT_LOG');
+	log4js.clearAppenders();
+	log4js.loadAppender('file');
+	log4js.addAppender(log4js.appenders.file(log_path), 'BOT_LOG');
+}
+
+setInterval(checkTL, 500);
+
+function checkTL() {
+
 }
