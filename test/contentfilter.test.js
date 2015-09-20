84aedd9f0913d889544677887874a9e250b62bae
sardo.ip <sardo.ip@nxtomo.hk>
2015-09-20 14:55:00 +0800

    Complete the test case of contentfilter

--- test/contentfilter.test.js
+++ test/contentfilter.test.js
@@ -13,6 +13,8 @@ exports['Test verifiyKeyword'] = {
 
 		var result = filter.verifiyKeyword(list,content);
 
+		logger.debug('Keywords:' + content);
+
 		logger.debug('Response: ' + result);
 
 		test.ok(result !== null, 'It should return a string! ');
@@ -30,11 +32,64 @@ exports['Test verifiyKeyword'] = {
 
 		var result = filter.verifiyKeyword(list,content);
 
+		logger.debug('Keywords:' + content);
+
 		logger.debug('Response: ' + result);
 
 		test.ok(result !== null, 'It should return a string! ');
 
 		test.equal(result, 'Love', 'It should return Love!' );
+
+		test.done();
+	},
+
+	'Test keywords not found(no keywords in list)': function(test) {
+
+		var list = [];
+
+		var content = "Kagero";
+
+		var result = filter.verifiyKeyword(list,content);
+
+		logger.debug('Keywords:' + content);
+
+		logger.debug('Response: ' + result);
+
+		test.ok(!result, 'It should return undefined! ');
+
+		test.done();
+	},
+
+	'Test keywords not found(one keywords in list)': function(test) {
+
+		var list = [{"Hello":"World!"}];
+
+		var content = "Kagero";
+
+		var result = filter.verifiyKeyword(list,content);
+
+		logger.debug('Keywords:' + content);
+
+		logger.debug('Response: ' + result);
+
+		test.ok(!result, 'It should return undefined! ');
+
+		test.done();
+	},
+
+	'Test keywords not found(two keywords in list)': function(test) {
+
+		var list = [{"Hello":"World!","Kagero":"Love"}];
+
+		var content = "Kuroshio";
+
+		var result = filter.verifiyKeyword(list,content);
+
+		logger.debug('Keywords:' + content);
+
+		logger.debug('Response: ' + result);
+
+		test.ok(!result, 'It should return undefined! ');
 
 		test.done();
 	}
