3d448a1727587d659ebebc43077ac7fdd63a9c27
Sardo <sardo.ip@sardo.work>
2015-09-20 19:42:43 +0800

    Add test case of action.js

---
+++ test/action.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var logger = require('log4js').getLogger('Unit-Test');
+var PlurkClient = require('plurk').PlurkClient;
+var filter = require(__dirname + '/../ecplurkbot/lib/action');
+
+exports['Test respond'] = {
+
+	'Test add respond': function(test) {
+
+	}
+}
