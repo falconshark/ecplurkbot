64395f39b764ca2726fd69e65fab3c32d7f6b5d8
Sardo <sardo.ip@sardo.work>
2015-10-15 18:53:40 +0800

    Add comment and random reply function

--- ecplurkbot/lib/contentfilter.js
+++ ecplurkbot/lib/contentfilter.js
@@ -2,21 +2,41 @@ function verifiyKeyword(keywordList, content) {
 
 	for (var i = 0; i < keywordList.length; i++){
 
+		//從keywordList的Array裡提取關鍵字
+
 		var keyword = Object.keys(keywordList[i])[0];
+
+		//透過正規表達式搜尋關鍵字
 
 		var re = new RegExp(keyword, "gi");
 
 		var result = content.match(re);
 
+		//如輸入的內容裡有符合的關鍵字，則作出相應的回應
+
 		if(result){
 
 			var key = result[0];
 
-			var response = keywordList[i][key];
+			var minNumber = 0;
+
+			var maxNumber = keywordList[i][key].length - 1;
+
+			//隨機抽取一個數字，再透過此數字從回應的陣列裡取出對應的回應
+
+			var responseNumber = _getRandomInt(minNumber, maxNumber);
+
+			var response = keywordList[i][key][responseNumber];
 
 			return response;
 		}
 	}
+}
+
+function _getRandomInt(min, max) {
+
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
 
 module.exports = {
