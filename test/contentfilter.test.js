c288766c69dc113758bcde94d41a12f742c405e0
Sardo <sardo.ip@sardo.work>
2015-10-15 18:54:24 +0800

    Add test case of more than a response in keyword list

--- test/contentfilter.test.js
+++ test/contentfilter.test.js
@@ -5,9 +5,9 @@ var filter = require(__dirname + '/../ecplurkbot/lib/contentfilter');
 
 exports['Test verifiyKeyword'] = {
 
-	'Test keywords found(One keywords in list)': function(test) {
+	'Test keywords found(One keywords in list, one response)': function(test) {
 
-		var list = [{"Hello":"World!"}];
+		var list = [{"Hello":["World!"]}];
 
 		var content = "Hello World";
 
@@ -19,14 +19,14 @@ exports['Test verifiyKeyword'] = {
 
 		test.ok(result !== null, 'It should return a string! ');
 
-		test.equal(result, 'World!', 'It should return World!' );
+		test.equal(result, 'World!', 'Response should World!' );
 
 		test.done();
 	},
 
-	'Test keywords found(Two keywords in list)': function(test) {
+	'Test keywords found(Two keywords in list, one response)': function(test) {
 
-		var list = [{"Hello":"World!"},{"Kagero":"Love"}];
+		var list = [{"Hello":["World!"]},{"Kagero":["Love"]}];
 
 		var content = "Kagero";
 
@@ -38,7 +38,45 @@ exports['Test verifiyKeyword'] = {
 
 		test.ok(result !== null, 'It should return a string! ');
 
-		test.equal(result, 'Love', 'It should return Love!' );
+		test.equal(result, 'Love', 'Response should be Love!' );
+
+		test.done();
+	},
+
+	'Test keywords found(One keywords in list, two response)': function(test) {
+
+		var list = [{"Hello":["World!","Kumano!"]}];
+
+		var content = "Hello World";
+
+		var result = filter.verifiyKeyword(list, content);
+
+		logger.debug('Content:' + content);
+
+		logger.debug('Response: ' + result);
+
+		test.ok(result !== null, 'It should return a string! ');
+
+		test.ok(result === 'World!' || result === 'Kumano!', "Response should be World! or Kumano!");
+
+		test.done();
+	},
+
+	'Test keywords found(Two keywords in list, two response)': function(test) {
+
+		var list = [{"Hello":["World!","Kumano!"]},{"Kagero":["Love","Days"]}];
+
+		var content = "Kagero";
+
+		var result = filter.verifiyKeyword(list,content);
+
+		logger.debug('Keywords:' + content);
+
+		logger.debug('Response: ' + result);
+
+		test.ok(result !== null, 'It should return a string! ');
+
+		test.ok(result === 'Love' || result === 'Days', "Response should be Love or Days!");
 
 		test.done();
 	},
@@ -62,7 +100,7 @@ exports['Test verifiyKeyword'] = {
 
 	'Test keywords not found(one keywords in list)': function(test) {
 
-		var list = [{"Hello":"World!"}];
+		var list = [{"Hello":["World!"]}];
 
 		var content = "Kagero";
 
@@ -79,7 +117,7 @@ exports['Test verifiyKeyword'] = {
 
 	'Test keywords not found(two keywords in list)': function(test) {
 
-		var list = [{"Hello":"World!"},{"Kagero":"Love"}];
+		var list = [{"Hello":["World!"]},{"Kagero":["Love"]}];
 
 		var content = "Kuroshio";
 
